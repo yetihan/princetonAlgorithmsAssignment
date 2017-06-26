b2d4cdb502abc8664bb909dbcd6f5345dc3f9a43
yeti <h_hzhou@sina.cn>
2017-06-26 10:27:28 +0800

    简化 注释

--- assignment2/RandomizedQueue.java
+++ assignment2/RandomizedQueue.java
@@ -25,7 +25,7 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomizedQueue<Item> implements Iterable<Item>{
     private int N;
     private Object[] arr;
-    private int capacity = 8;
+    private int capacity = 1;
 
 
 
@@ -53,23 +53,6 @@ public class RandomizedQueue<Item> implements Iterable<Item>{
             return (Item) randomArr[k++];
         }
 
-        /**
-         * Removes from the underlying collection the last element returned
-         * by this iterator (optional operation).  This method can be called
-         * only once per call to {@link #next}.  The behavior of an iterator
-         * is unspecified if the underlying collection is modified while the
-         * iteration is in progress in any way other than by calling this
-         * method.
-         *
-         * @throws UnsupportedOperationException if the {@code remove}
-         *                                       operation is not supported by this iterator
-         * @throws IllegalStateException         if the {@code next} method has not
-         *                                       yet been called, or the {@code remove} method has already
-         *                                       been called after the last call to the {@code next}
-         *                                       method
-         * @implSpec The default implementation throws an instance of
-         * {@link UnsupportedOperationException} and performs no other action.
-         */
         @Override
         public void remove() {
             throw  new java.lang.UnsupportedOperationException();
